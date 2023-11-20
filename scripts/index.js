8e6001f2de98fe83cd4d88e1ec210f8407eafe02
Lauren Blue <kupt5ova.tatyana@yandex.ru>
2023-11-21 00:09:46 +0100

    fix for review nr2

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const contentContainer = document.querySelector(".places__list"); // DOM узлы
 
-function addCard(cardData, handleDelete) {
+function createCard(cardData, handleDeleteCard) {
   const template = document.querySelector("#card-template").content;
-  const templateElement = template
-    .querySelector(".card")
-    .cloneNode(true); //шаг 1: клонируем шаблон
-
-  contentContainer.append(templateElement); //шаг 2: выводим карточки на страницу
+  const templateElement = template.querySelector(".card").cloneNode(true); //шаг 1: клонируем шаблон
 
   templateElement.querySelector(".card__title").textContent = cardData.name;
   templateElement.querySelector(".card__image").src = cardData.link;
   templateElement.querySelector(".card__image").alt = cardData.name; //шаг 3: устанавливаем значение вложенных элементов
 
   const deleteButton = templateElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", (e) => handleDelete(e.target)); //шаг 4: добавляем обработчик по клику
+  deleteButton.addEventListener("click", (e) => handleDeleteCard(e.target)); //шаг 4: добавляем обработчик по клику
 
   return templateElement;
 }
 
-function handleDelete(deleteButton) {
+function renderCard(createCard) {
+  contentContainer.prepend(createCard);
+} //исправлено замечание, добавление на страницу сделано отдельной функцией
+
+function handleDeleteCard(deleteButton) {
   const item = deleteButton.closest(".card");
   item.remove();
 } // функция удаления
 
 initialCards.forEach((card) => {
-  addCard(card, handleDelete);
+  renderCard(createCard(card, handleDeleteCard));
 });
