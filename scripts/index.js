c61a0925cf50ab8e72636bd59bf8b110dcff98c5
Lauren Blue <kupt5ova.tatyana@yandex.ru>
2023-11-20 11:31:33 +0100

    review nr 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const contentContainer = document.querySelector(".places__list"); // DOM узлы
 
-// @todo: DOM узлы
+function addCard(cardData, handleDelete) {
+  const template = document.querySelector("#card-template").content;
+  const templateElement = template
+    .querySelector(".card")
+    .cloneNode(true); //шаг 1: клонируем шаблон
 
-// @todo: Функция создания карточки
+  contentContainer.append(templateElement); //шаг 2: выводим карточки на страницу
 
-// @todo: Функция удаления карточки
+  templateElement.querySelector(".card__title").textContent = cardData.name;
+  templateElement.querySelector(".card__image").src = cardData.link;
+  templateElement.querySelector(".card__image").alt = cardData.name; //шаг 3: устанавливаем значение вложенных элементов
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = templateElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", (e) => handleDelete(e.target)); //шаг 4: добавляем обработчик по клику
+
+  return templateElement;
+}
+
+function handleDelete(deleteButton) {
+  const item = deleteButton.closest(".card");
+  item.remove();
+} // функция удаления
+
+initialCards.forEach((card) => {
+  addCard(card, handleDelete);
+});
